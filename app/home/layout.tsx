c48c0533d988784f09d30eed43b27cc5b4b1a788
Joseph Bangoura <ptijjo@hotmail.com>
2025-11-03 20:38:00 +0100

    update

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -3,8 +3,6 @@ import { User } from '@/interface/user.interface'
 import { Url } from '@/lib/Url'
 import axios from 'axios'
 import React, { ReactNode, useEffect, useState } from 'react'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
 import { useRouter } from 'next/navigation'
 
 const Layout = ({ children }: { children: ReactNode }) => {
@@ -27,16 +25,14 @@ const Layout = ({ children }: { children: ReactNode }) => {
         connected();
     }, [navigate])
 
-   
+
     console.log("user : ", user)
 
 
 
     return (
         <div className="flex flex-col flex-grow items-center justify-center w-full ">
-            {user && <Header user={user} />}
             {children}
-            <Footer />
         </div>)
 }
 
